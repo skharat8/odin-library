6188b29a37aa8959f9f360a6ad2e0ae20a5311fd
enigma9119 <enigma9119@users.noreply.github.com>
2023-10-22 16:21:27 -0700

    Design the individual book cards
    Add JS code to create each book and add it to the DOM. Add styling for
    the card element in the CSS. Use Roboto font for all card text.

--- script.js
+++ script.js
@@ -15,53 +15,94 @@ function Book(title, author, pages, isRead) {
 }
 
 function closeAddBookModal(e) {
-  const dialogDimensions = dialog.getBoundingClientRect();
+  const modalDimensions = modal.getBoundingClientRect();
   if (
-    e.clientX < dialogDimensions.left ||
-    e.clientX > dialogDimensions.right ||
-    e.clientY < dialogDimensions.top ||
-    e.clientY > dialogDimensions.bottom
+    e.clientX < modalDimensions.left ||
+    e.clientX > modalDimensions.right ||
+    e.clientY < modalDimensions.top ||
+    e.clientY > modalDimensions.bottom
   ) {
-    dialog.close();
+    modal.close();
   }
 }
 
 function addBookToLibrary(e) {
-  const title = document.querySelector("input[id='title']").value;
-  const author = document.querySelector("input[id='author']").value;
-  const pages = document.querySelector("input[id='pages']").value;
-  const isRead = document.querySelector("input[id='isRead']").value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const isRead = document.getElementById("isRead").checked;
 
   const book = new Book(title, author, pages, isRead);
   myLibrary.push(book);
+  createBookCard(book);
 }
 
 function displayAllBooks() {
   myLibrary.forEach(book => {
-    console.log(book);
+    createBookCard(book);
   });
+}
+
+function createBookCard(book) {
+  // Create new HTML elements
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+
+  const titleElement = document.createElement("span");
+  const authorElement = document.createElement("span");
+  const pagesElement = document.createElement("span");
+
+  const actionsDiv = document.createElement("div");
+  const readButton = document.createElement("button");
+  const removeButton = document.createElement("button");
+
+  // Define the styling for various elements
+  actionsDiv.style.display = "flex";
+  actionsDiv.style.gap = "1rem";
+  titleElement.style.fontWeight = "bold";
+
+  // Specify the text content
+  titleElement.textContent = `${book.title}`;
+  authorElement.textContent = `by ${book.author}`;
+  pagesElement.textContent = `${book.pages} pages`;
+  readButton.textContent = book.isRead ? "Read" : "Not Read";
+  removeButton.textContent = "Remove";
+
+  if (book.isRead) {
+    readButton.style.backgroundColor = "seagreen";
+  } else {
+    readButton.style.backgroundColor = "firebrick";
+  }
+
+  // Attach elements to the DOM
+  bookCard.appendChild(titleElement);
+  bookCard.appendChild(authorElement);
+  bookCard.appendChild(pagesElement);
+  actionsDiv.appendChild(readButton);
+  actionsDiv.appendChild(removeButton);
+  bookCard.appendChild(actionsDiv);
+
+  booksContainer.appendChild(bookCard);
 }
 
 const form = document.querySelector("#add-book-form");
 const modal = document.querySelector("dialog");
 
-const addBookModalButton = document.querySelector(".add-book-modal");
-addBookModalButton.addEventListener("click", e => modal.showModal());
+const openBookModalButton = document.querySelector(".add-book-modal");
+openBookModalButton.addEventListener("click", e => modal.showModal());
 
 modal.addEventListener("click", closeAddBookModal);
 modal.addEventListener("close", e => form.reset());
 
 // Add user provided book to the library
+const booksContainer = document.querySelector(".books-container");
 const addBookButton = document.querySelector("#add-book");
 addBookButton.addEventListener("click", addBookToLibrary);
 
 // Add some default books to the library
 const defaultBook1 = new Book("The Hobbit", "Tolkien", 295, false);
 const defaultBook2 = new Book("Dune", "Frank Herbert", 658, false);
-const defaultBook3 = new Book(
-  "Peril at End House",
-  "Agatha Christie",
-  287,
-  true
-);
+const defaultBook3 = new Book("The Big Four", "Agatha Christie", 272, true);
+
 myLibrary.push(defaultBook1, defaultBook2, defaultBook3);
+displayAllBooks();
