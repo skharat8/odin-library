63437f76234a37197eb5d5987390f179f9ae489e
enigma9119 <enigma9119@users.noreply.github.com>
2023-10-21 21:18:00 -0700

    Design the modal to add a new book
    Add HTML/CSS for how the modal looks. Add JS code to close the modal on
    event click outside the dialog box.

--- script.js
+++ script.js
@@ -14,7 +14,23 @@ function Book(title, author, pages, isRead) {
   };
 }
 
-function addBookToLibrary(title, author, pages, isRead) {
+function showAddBookModal(e) {
+  dialog.showModal();
+}
+
+function closeAddBookModal(e) {
+  const dialogDimensions = dialog.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialog.close();
+  }
+}
+
+function addBookToLibrary(e) {
   const book = new Book(title, author, pages, isRead);
   myLibrary.push(book);
 }
@@ -28,3 +44,12 @@ function displayAllBooks() {
 addBookToLibrary("The Hobbit", "Tolkien", 295, false);
 addBookToLibrary("The GE", "asa", 35, true);
 addBookToLibrary("The ASFAFA", "www", 311, false);
+
+const addBookModalButton = document.querySelector(".add-book-modal");
+addBookModalButton.addEventListener("click", showAddBookModal);
+
+const dialog = document.querySelector("dialog");
+dialog.addEventListener("click", closeAddBookModal);
+
+const addBookButton = document.querySelector(".add-book");
+addBookButton.addEventListener("click", addBookToLibrary);
