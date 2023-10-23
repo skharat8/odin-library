e4dbb79218a89a3612c644e37695fb19f322271f
enigma9119 <enigma9119@users.noreply.github.com>
2023-10-22 20:47:42 -0700

    Add prototype functions for Book
    Also, update the isRead attribute in the Book object when the read
    status is toggled.

--- script.js
+++ script.js
@@ -5,14 +5,25 @@ function Book(title, author, pages, isRead) {
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
-
-  this.info = function () {
-    let info =
-      this.title + " by " + this.author + ", " + this.pages + " pages, ";
-    info += isRead ? "read" : "not read yet";
-    return info;
-  };
 }
+
+Book.prototype.getInfo = function () {
+  let info = this.title + " by " + this.author + ", " + this.pages + " pages, ";
+  info += isRead ? "read" : "not read yet";
+  return info;
+};
+
+Book.prototype.toggleReadStatus = function (e) {
+  if (e.target.textContent === "Read") {
+    e.target.style.backgroundColor = "firebrick";
+    e.target.textContent = "Not Read";
+  } else {
+    e.target.style.backgroundColor = "seagreen";
+    e.target.textContent = "Read";
+  }
+
+  this.isRead = !this.isRead;
+};
 
 function closeAddBookModal(e) {
   const modalDimensions = modal.getBoundingClientRect();
@@ -76,7 +87,7 @@ function createBookCard(book, libraryIndex) {
     readButton.style.backgroundColor = "firebrick";
   }
 
-  readButton.addEventListener("click", toggleReadStatus);
+  readButton.addEventListener("click", book.toggleReadStatus.bind(book));
   removeButton.addEventListener("click", removeBook);
 
   // Attach elements to the DOM
@@ -88,16 +99,6 @@ function createBookCard(book, libraryIndex) {
   bookCard.appendChild(actionsDiv);
 
   booksContainer.appendChild(bookCard);
-}
-
-function toggleReadStatus(e) {
-  if (e.target.textContent === "Read") {
-    e.target.style.backgroundColor = "firebrick";
-    e.target.textContent = "Not Read";
-  } else {
-    e.target.style.backgroundColor = "seagreen";
-    e.target.textContent = "Read";
-  }
 }
 
 function removeBook(e) {
